1f1bb503e0dea1365b60e6ff88fe2a0b890d34c8
Andrew Jensen <andyjensen@email.com>
2020-06-11 03:19:14 -0500

    Updates for Windows packaging

--- installer/hook-z3c.rml.py
+++ installer/hook-z3c.rml.py
@@ -6,3 +6,14 @@ datas = copy_metadata('z3c.rml')
 # is missing this. Without it, we get a "pkg_resources.DistributionNotFound" runtime error saying:
 #    The 'google-api-python-client' distribution was not found and is required by the application
 datas += copy_metadata('google-api-python-client')
+
+# And these are needed on Windows, for some reason.
+datas += copy_metadata('backports.tempfile')
+datas += copy_metadata('lxml')
+datas += copy_metadata('pygments')
+datas += copy_metadata('PyPDF2')
+datas += copy_metadata('reportlab')
+datas += copy_metadata('setuptools')
+datas += copy_metadata('six')
+datas += copy_metadata('zope.interface')
+datas += copy_metadata('zope.schema')
